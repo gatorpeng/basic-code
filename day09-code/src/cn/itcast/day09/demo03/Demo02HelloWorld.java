800d27f3c4be576439998628a828b751b27fb1cf
gatorpeng <xinlaide1984@gmail.com>
2019-08-09 09:14:17 -0400

    Add hello world message to project

--- day09-code/src/cn/itcast/day09/demo03/Demo02HelloWorld.java
+++ day09-code/src/cn/itcast/day09/demo03/Demo02HelloWorld.java
@@ -4,5 +4,6 @@ public class Demo02HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Please keep yourself down.");
     }
 }
